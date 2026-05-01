7f964a7c58f03abe5a774288030f44b4ac448598
surjanthakur <tsurjan506@gmail.com>
2026-05-01 21:38:40 +0530

    save

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaDiscord, FaXTwitter } from "react-icons/fa6";
+import SocialLinks from "../components/SocialLinks.jsx";
 
 const Footer = () => {
   return (
@@ -6,40 +6,14 @@ const Footer = () => {
       <span className="absolute top-0 left-0 h-0.5 w-full bg-linear-to-r from-transparent via-green-400 to-transparent"></span>
       {/* Large CIFRAR Text Background */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-[28vw] font-bold tracking-tight bg-linear-to-r from-green-300 to-emerald-400 bg-clip-text text-transparent">
+        <h1 className="text-[25vw] font-bold tracking-tight bg-linear-to-r from-green-300 to-emerald-400 bg-clip-text text-transparent">
           CIFRAR
         </h1>
       </div>
 
       {/* Social Icons at Top Right */}
-      <div className="absolute top-4 right-6 flex gap-6">
-        <a
-          href="#"
-          className="group text-gray-400 hover:text-emerald-400 transition-all duration-300 hover:scale-105"
-          aria-label="GitHub"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub className="text-2xl" />
-        </a>
-        <a
-          href="#"
-          className="group text-gray-400 hover:text-emerald-400 transition-all duration-300 hover:scale-105"
-          aria-label="X Profile"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaXTwitter className="text-xl" />
-        </a>
-        <a
-          href="#"
-          className="group text-gray-400 hover:text-emerald-400 transition-all duration-300 hover:scale-105"
-          aria-label="Discord"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaDiscord className="text-2xl" />
-        </a>
+      <div className="absolute bottom-4 right-1 flex gap-6">
+        <SocialLinks />
       </div>
 
       {/* Means to Encrypt at Bottom Center */}
