1ef8c537b7009752efa6b7d540032db6870037cf
surjanthakur <tsurjan506@gmail.com>
2026-05-01 14:39:37 +0530

    s

--- frontend/src/pages/ExportFiles.js
+++ frontend/src/pages/ExportFiles.js
@@ -1,6 +1,6 @@
-import { lazy } from 'react'
-const HomePage = lazy(() => import('./HomePage'))
-const CreateRoomForm = lazy(() => import('./CreateRoomFormPage'))
-const JoinRoomForm = lazy(() => import('./JoinRoomFormPage'))
+import { lazy } from "react";
+const HomePage = lazy(() => import("./HomePage"));
+const CreateRoomForm = lazy(() => import("./CreateRoomFormPage"));
+const JoinRoomForm = lazy(() => import("./JoinRoomFormPage"));
 
-export { HomePage, CreateRoomForm, JoinRoomForm }
+export { HomePage, CreateRoomForm, JoinRoomForm };
