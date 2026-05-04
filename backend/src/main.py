95b59274e3f28a0e38fd9c793dc8cdd0ab6a5665
surjanthakur <tsurjan506@gmail.com>
2026-05-04 21:48:31 +0530

    save

--- backend/src/main.py
+++ backend/src/main.py
@@ -16,14 +16,28 @@ logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    """
+    Lifespan context manager for the FastAPI application.
+
+    Ensures Redis connection is available at startup and properly
+    closed during application shutdown.
+
+    Args:
+        app (FastAPI): The FastAPI application instance.
+
+    Yields:
+        None
+    Raises:
+        RuntimeError: If there is an error during app startup related to Redis connection.
+    """
+
     try:
         await check_redis_connection()
-    except Exception:
-        raise
-    yield
-    # Cleanup on shutdown
-
-    await close_redis_connection()
+        yield
+    except Exception as err:
+        raise RuntimeError(f"error on startup app {err}")
+    finally:
+        close_redis_connection()
 
 
 app = FastAPI(lifespan=lifespan, title="cifrar-chat", version="0.1")
@@ -31,6 +45,7 @@ app = FastAPI(lifespan=lifespan, title="cifrar-chat", version="0.1")
 
 @app.get("/health")
 def check_health():
+    """Health check endpoint to verify if the application is running."""
     return {"status": "ok"}
 
 
@@ -38,6 +53,16 @@ def check_health():
 async def log_request(
     request: Request, call_next: Callable[[Request], Awaitable[Response]]
 ) -> Response:
+    """
+    Middleware to log the details and processing time of each HTTP request.
+
+    Args:
+        request (Request): The incoming HTTP request.
+        call_next (Callable[[Request], Awaitable[Response]]): Function to process the request and get a response.
+
+    Returns:
+        Response: The HTTP response with an added "x-process-time" header containing the processing duration.
+    """
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
@@ -48,5 +73,5 @@ async def log_request(
     return response
 
 
-# routes
+# app routers
 app.include_router(room_router.Router, prefix="/api")
