8ba7fe37f5a2bab9e3ec88ec3eb6fe31ed4c9161
surjanthakur <tsurjan506@gmail.com>
2026-05-14 18:52:42 +0530

    s

--- backend/src/utils/rooms_utils.py
+++ backend/src/utils/rooms_utils.py
@@ -16,13 +16,15 @@ def generate_room_key():
 
 class ManageUserStore:
     """
-    store the new user info in redis.
+    ManageUserStore handles storage and management of user-related data in Redis.
     """
 
     def __init__(self):
+        """
+        Initializes the ManageUserStore class.
+        """
         pass
 
-    # store users info
     async def add_user(
         self,
         username: str,
@@ -31,6 +33,9 @@ class ManageUserStore:
         access_key: str,
         connection_id: str,
     ):
+        """
+        Store the user's info in Redis under 'user:{user_id}' hash.
+        """
         await redis_client.hset(
             name=f"user:{user_id}",
             mapping={
@@ -42,18 +47,24 @@ class ManageUserStore:
         )
         await redis_client.hexpire(name=f"users:{user_id}", seconds=7200)
 
-    # store room:users
     async def add_user_in_room(self, room_id: str, user_id: str):
+        """
+        Add a user to the set of users in a specified room in Redis.
+        """
         await redis_client.sadd(f"room:{room_id}:users", user_id)
         await redis_client.expire(f"room:{room_id}:users", 7200)
 
-    # store users:connections
     async def add_users_connections(self, user_id: str, connection_id: str):
+        """
+        Add a connection ID to a user's connections set in Redis.
+        """
         await redis_client.sadd(f"users:{user_id}:connections", connection_id)
         await redis_client.expire(f"users:{user_id}:connections", 7200)
 
-    # store connections_info
     async def add_connection(self, connection_id: str, user_id: str, room_id: str):
+        """
+        Store connection information, mapping a connection ID to a user and room.
+        """
         await redis_client.hset(
             name=f"connection:{connection_id}",
             mapping={
